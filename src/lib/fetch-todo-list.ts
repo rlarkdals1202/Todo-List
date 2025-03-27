366ece884477864824f0eaecd726d8b86d2cb4d9
Kim Kangmin <rlarkdals1202@gmail.com>
2025-03-27 23:37:20 +0900

    Comment: 함수에 대한 주석 추가

--- src/lib/fetch-todo-list.ts
+++ src/lib/fetch-todo-list.ts
@@ -1,6 +1,13 @@
 import ky from 'ky';
 import type Todo from '@/types/todo.interface';
 
+/**
+ * 할 일 목록을 가져오는 async 함수입니다.
+ *
+ * 할 일 목록을 성공적으로 가져오면, 할 일 목록을 반환합니다.
+ * 할 일 목록을 가져오는 데 실패하면, 에러를 콘솔에 출력하고, 빈 배열을 반환합니다.
+ *
+ */
 const fetchTodoList = async () => {
     try {
         const todoList = await ky
