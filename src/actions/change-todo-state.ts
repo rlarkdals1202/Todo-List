daa402bcf1d1ceb49429319c293d7d4a7e91c347
Kim Kangmin <rlarkdals1202@gmail.com>
2025-03-27 18:32:05 +0900

    Comment: 서버 액션에 대한 주석 추가

--- src/actions/change-todo-state.ts
+++ src/actions/change-todo-state.ts
@@ -3,6 +3,13 @@
 import ky from 'ky';
 import { revalidateTag } from 'next/cache';
 
+/**
+ * 할 일의 상태를 변경하는 서버 액션입니다.
+ *
+ * 상태를 성공적으로 변경하였다면, 할 일 목록 데이터를 무효화합니다.
+ * 상태를 변경하는 데 실패하였다면, 에러를 콘솔에 출력합니다.
+ *
+ */
 const changeTodoState = async ({ id, isCompleted }: { id: number; isCompleted: boolean }) => {
     try {
         await ky.patch(`${process.env.NEXT_PUBLIC_API_URL}/items/${id}`, {
--- src/actions/create-todo.ts
+++ src/actions/create-todo.ts
@@ -3,6 +3,13 @@
 import ky from 'ky';
 import { revalidateTag } from 'next/cache';
 
+/**
+ * 할 일을 생성하는 서버 액션입니다.
+ *
+ * 할 일을 성공적으로 생성하였다면, 할 일 목록 데이터를 무효화하고, 생성 성공 관련 객체를 반환합니다.
+ * 할 일을 생성하는 데 실패하였다면, 에러를 콘솔에 출력하고, 생성 실패 관련 객체를 반환합니다.
+ *
+ */
 const createTodo = async (_: any, formData: FormData) => {
     const todo = formData.get('todo')?.toString();
 
