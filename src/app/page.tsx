4e5e6461f70ea372ed6d418e5b31cbefeebce938
Kim Kangmin <rlarkdals1202@gmail.com>
2025-03-27 23:24:15 +0900

    Feat: 메인 페이지 컴포넌트 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,52 @@
-import Image from "next/image";
+import AddTodoForm from '@/components/home/add-todo-form';
+import Image from 'next/image';
+import EmptyListMessage from '@/components/home/empty-list-message';
+import TodoList from '@/components/home/todo-list';
+import fetchTodoList from '@/lib/fetch-todo-list';
 
-export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+/**
+ * 메인 페이지 컴포넌트입니다.
+ */
+const Page = async () => {
+    // 서버로부터 할 일 목록을 가져옵니다.
+    const todoList = await fetchTodoList();
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
-}
+    return (
+        <main className={'flex min-h-[calc(100dvh-3.75rem)] flex-1 flex-col bg-gray-50'}>
+            <div className={'mx-4 mt-4 flex flex-grow flex-col gap-y-6 md:mx-6 md:mt-6 md:gap-y-10 lg:mx-28 3xl:mx-[360px]'}>
+                <AddTodoForm />
+                <div className={'flex flex-col gap-y-12 lg:flex-row lg:gap-x-6'}>
+                    <div className={'w-full lg:w-1/2 lg:flex-col lg:gap-y-[4rem]'}>
+                        <Image src={'/images/text/todo.png'} alt={'todo-text'} width={101} height={36} />
+                        {todoList.filter(todo => !todo.isCompleted).length === 0 ? (
+                            <EmptyListMessage listType={'todo'}>
+                                <p className={'text-center font-bold text-slate-400'}>
+                                    할 일이 없어요. <br />
+                                    TODO를 새롭게 추가해주세요!
+                                </p>
+                            </EmptyListMessage>
+                        ) : (
+                            <TodoList todos={todoList.filter(todo => !todo.isCompleted)} />
+                        )}
+                    </div>
+                    <div className={'w-full lg:w-1/2 lg:flex-col lg:gap-y-[4rem]'}>
+                        <Image src={'/images/text/done.png'} alt={'done-text'} width={101} height={36} />
+                        {todoList.filter(todo => todo.isCompleted).length === 0 ? (
+                            <EmptyListMessage listType={'done'}>
+                                <p className={'text-center font-bold text-slate-400'}>
+                                    아직 다 한 일이 없어요.
+                                    <br />
+                                    해야 할 일을 체크해보세요!
+                                </p>
+                            </EmptyListMessage>
+                        ) : (
+                            <TodoList todos={todoList.filter(todo => todo.isCompleted)} />
+                        )}
+                    </div>
+                </div>
+            </div>
+        </main>
+    );
+};
+
+export default Page;
