8b4bffa2bfa508f81798d596f9b189e27f968d04
Kim Kangmin <rlarkdals1202@gmail.com>
2025-03-30 00:44:45 +0900

    Style: GNB 패딩 상하 간격 수정

--- src/components/global-navigation-bar.tsx
+++ src/components/global-navigation-bar.tsx
@@ -9,7 +9,7 @@ const GlobalNavigationBar = () => {
     return (
         <nav
             className={
-                '3xl:px-[18.75%] flex h-full w-full border-b border-slate-200 px-4 pt-[9.71px] pb-[10.29px] md:px-6 lg:px-36'
+                '3xl:px-[18.75%] flex h-full w-full border-b border-slate-200 px-4 py-[10px] md:px-6 lg:px-36'
             }
         >
             <Link href='/'>
